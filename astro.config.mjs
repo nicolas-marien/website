16b152b94ca1883fe2091959c2771d0c6c8ac4ae
Nicolas Marien <mariennicolas@icloud.com>
2025-01-27 15:22:50 +0100

    chore: add tailwind integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import { defineConfig } from "astro/config";
 
 import mdx from "@astrojs/mdx";
 
+import tailwind from "@astrojs/tailwind";
+
 export default defineConfig({
-  integrations: [mdx()],
+  integrations: [mdx(), tailwind()],
 });
