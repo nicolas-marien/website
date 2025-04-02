267a5c72b8ae0bc410b1fbc78ad482acedc706f7
Nicolas Marien <mariennicolas@icloud.com>
2025-04-02 21:27:16 +0200

    style: apply some basic styling

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,17 @@
 // @ts-check
-import { defineConfig } from "astro/config";
-import remarkCallout from "@r4ai/remark-callout";
+import { defineConfig } from 'astro/config';
+import remarkCallout from '@r4ai/remark-callout';
 
-import mdx from "@astrojs/mdx";
+import mdx from '@astrojs/mdx';
 
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   integrations: [mdx(), tailwind()],
   markdown: {
     remarkPlugins: [remarkCallout],
+    shikiConfig: {
+      theme: 'tokyo-night',
+    },
   },
 });
