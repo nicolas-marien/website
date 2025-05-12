5a6e47c4ecd21395bf0ccb876f3092123e99081f
Nicolas Marien <mariennicolas@icloud.com>
2025-05-12 21:35:14 +0200

    feat: add rss

---
+++ src/pages/feed.xml.ts
@@ -0,0 +1,28 @@
+import rss, { type RSSFeedItem } from '@astrojs/rss';
+import { type APIContext } from 'astro';
+import { getCollection } from 'astro:content';
+
+export async function GET(context: APIContext) {
+  const posts = await getCollection('blog');
+  return rss({
+    title: 'Nicolas Marien - Blog',
+    // `<description>` field in output xml
+    description: 'Some personal archives about software development. Maybe more.',
+    // Pull in your project "site" from the endpoint context
+    // https://docs.astro.build/en/reference/api-reference/#site
+    site: context.site!,
+    // Array of `<item>`s in output xml
+    // See "Generating items" section for examples using content collections and glob imports
+    items: posts.map<RSSFeedItem>((post) => {
+      return {
+        title: post.data.title,
+        pubDate: new Date(post.data.created_at),
+        description: post.data.abstract,
+        // Compute RSS link from post `id`
+        // This example assumes all posts are rendered as `/blog/[id]` routes
+        link: `/posts/${post.id}/`,
+      };
+    }),
+    customData: `<language>en-us</language>`,
+  });
+}
