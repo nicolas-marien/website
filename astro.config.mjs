cfc6811f838eab5d5e681f4eae8a4f260e234007
Nicolas Marien <mariennicolas@icloud.com>
2025-08-19 22:07:17 +0200

    chore: add link icon on heading

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,5 @@
 // @ts-check
+import { fromHtmlIsomorphic } from 'hast-util-from-html-isomorphic'
 import { defineConfig } from 'astro/config';
 import remarkCallout from '@r4ai/remark-callout';
 import { rehypeHeadingIds } from '@astrojs/markdown-remark';
@@ -21,7 +22,19 @@ export default defineConfig({
   markdown: {
     remarkPlugins: [remarkCallout],
     rehypePlugins: [rehypeHeadingIds,
-      rehypeAutolinkHeadings
+      [
+        rehypeAutolinkHeadings,
+        {
+          behavior: 'append',
+          properties: {
+            className: ['heading-anchor'],
+          },
+          content: fromHtmlIsomorphic(
+            '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-link-icon lucide-link"><path d="M10 13a5 5 0 0 0 7.54.54l3-3a5 5 0 0 0-7.07-7.07l-1.72 1.71"/><path d="M14 11a5 5 0 0 0-7.54-.54l-3 3a5 5 0 0 0 7.07 7.07l1.71-1.71"/></svg><span class="sr-only">Link to heading</span>',
+            { fragment: true },
+          ).children,
+        },
+      ],
     ]
   },
 
